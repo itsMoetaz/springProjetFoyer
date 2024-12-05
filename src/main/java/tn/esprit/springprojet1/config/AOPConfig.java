133648fb33eeb5046e08aeac581d53c82b12f369
moetaz <moetaz.benkhedher@esprit.tn>
2024-12-05 11:13:29 +0100

    AOP Config

---
+++ src/main/java/tn/esprit/springprojet1/config/AOPConfig.java
@@ -0,0 +1,18 @@
+package tn.esprit.springprojet1.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+@Component
+@Aspect
+@Slf4j
+public class AOPConfig {
+    @Before("execution(* tn.esprit.springprojet1.services.*.*(..))")
+    public void logMethodEntry(JoinPoint joinPoint) {
+        String name = joinPoint.getSignature().getName();
+        log.info("In method " + name + " : ");
+    }
+
+}
